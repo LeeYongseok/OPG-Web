e2702fa318be0798b406fdeef3e2cf4282107f2f
reussite <sho6368@naver.com>
2017-02-22 23:47:43 +0900

    Add 'info'route

--- OPG/app.js
+++ OPG/app.js
@@ -14,7 +14,7 @@ var photos = require('./routes/photos');
 var signUp = require('./routes/signUp');
 var about=require('./routes/about');
 var posts=require('./routes/posts');
-
+var info=require('./routes/info');
 
 var app = express();
 var mongoose=require('mongoose');
@@ -40,7 +40,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit:'5mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
@@ -60,6 +60,8 @@ app.use('/photo', photos);
 
 //소개
 app.use('/about', about);
+app.use('/info', info);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
